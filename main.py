90a27390a8b25a37af8a576ae151dc8d099cfa60
wasabina67 <wasabina67@gmail.com>
2025-12-16 20:09:41 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import os
+
+from googleapiclient.discovery import build
+
+
+def generate_markdown(videos):
+    print(videos)
+
+
+def get_trending_videos(api_key):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    request = youtube.videos().list(
+        part="snippet,statistics",
+        chart="mostPopular",
+        regionCode='JP',
+        maxResults=10,
+    )
+    return request.execute().get('items', [])
+
+
 def main():
-    print("Hello from youtube-trends-archive!")
+    api_key = os.environ.get('YOUTUBE_API_KEY')
+    if not api_key:
+        print("Error: YOUTUBE_API_KEY is not set")
+        return
+
+    try:
+        print("Fetching trending videos...")
+        videos = get_trending_videos(api_key)
+
+        generate_markdown(videos)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
